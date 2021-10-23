5ca794d09dbf4ae17a9127395c2b92cf8f8ca209
Ryan O'Neill <r@ryanoneill.com>
2021-10-23 19:38:49 +0100

    Parse response from login.

---
+++ AlcatelRouterStats/Models/RequestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace AlcatelRouterStats.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RequestBase
+    {
+    }
+}
